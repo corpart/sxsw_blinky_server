62a8ddde9233bafca0dbfa5d60ac31549f706439
michael philetus weller <philetus@gmail.com>
2017-02-03 14:00:10 -0800

    made simple udp debug server & client in examples dir

---
+++ examples/udp_debug_client.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net"
+)
+
+// sends an example json message over udp to localhost:3333 and exits
+func main() {
+
+  msg := "{\"source\": \"17\", \"flavor\": \"start_touch\", \"choice\": \"right\"}"
+
+  // connect to local udp server on port 3333
+  conn, err := net.Dial("udp", "127.0.0.1:3333")
+  if err != nil {
+  	log.Fatal(err)
+  }
+  defer conn.Close()
+
+  // write message to server
+  _, err = conn.Write([]byte(msg))
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Printf("wrote %v to local server on port 3333", msg)
+}
