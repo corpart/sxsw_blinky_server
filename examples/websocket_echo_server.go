b38513f4b68e24fb8eef59374424be4a64fcbef0
michael philetus weller <philetus@gmail.com>
2017-02-18 00:27:55 -0800

    cleaned up websocket example

--- examples/websocket_echo_server.go
+++ examples/websocket_echo_server.go
@@ -10,11 +10,15 @@ import (
 func Echo(ws *websocket.Conn) {
     var err error
 
+    clntaddr := ws.Request().RemoteAddr
+
+    fmt.Println("opened socket for client at " + clntaddr)
+
     for {
         var reply string
 
         if err = websocket.Message.Receive(ws, &reply); err != nil {
-            fmt.Println("Can't receive")
+            fmt.Println("Can't receive from client at " + clntaddr)
             break
         }
 
@@ -32,6 +36,8 @@ func Echo(ws *websocket.Conn) {
 
 func main() {
     http.Handle("/", websocket.Handler(Echo))
+
+    fmt.Println("listening for websocket clients at ws://localhost:8888")
 
     if err := http.ListenAndServe(":8888", nil); err != nil {
         log.Fatal("ListenAndServe:", err)
